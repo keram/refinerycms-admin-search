96b086b3c41974cf23cde21da572d707a197ac5e
Marek Labos <keraml@gmail.com>
2014-01-24 21:04:05 +0100

    reducing number of sql requests by including translations

--- app/decorators/controllers/refinery/admin/images_controller_decorator.rb
+++ app/decorators/controllers/refinery/admin/images_controller_decorator.rb
@@ -1,7 +1,7 @@
 begin
   Refinery::Admin::ImagesController.class_eval do
     def search_all_images
-      @images = search_all_records @images
+      @images = search_all_records(@images).includes(:translations)
     end
   end
 rescue NameError
--- app/decorators/controllers/refinery/admin/pages_controller_decorator.rb
+++ app/decorators/controllers/refinery/admin/pages_controller_decorator.rb
@@ -1,7 +1,7 @@
 begin
   Refinery::Admin::PagesController.class_eval do
     def search_all_pages
-      @pages = search_all_records @pages
+      @pages = search_all_records(@pages).includes(:translations)
     end
   end
 rescue NameError
